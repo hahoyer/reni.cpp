4b1d456abd36cf11b1539b00377a6603079c66fa
hahoyer <hahoyer@gmail.com>
2014-01-23 10:01:12 +0100

    New: Compiler

--- Reni/Admin/Import.h
+++ Reni/Admin/Import.h
@@ -1,2 +1,3 @@
 #pragma once
 #include "../../HWLib/Admin/Export.h"
+using namespace HWLib;
--- Reni/Compiler.h
+++ Reni/Compiler.h
@@ -1,6 +1,15 @@
 #pragma once
 
-class Compiler
+namespace Reni
 {
-};
-
+    class Compiler
+    {
+        class internal;
+        internal const & _internal;
+    public:
+        Compiler() = delete;
+        Compiler(Compiler const&) = delete;
+        Compiler(String const&fileName);
+        ~Compiler();
+    };
+}
