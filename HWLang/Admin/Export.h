860bdac94a2e5b0406ac8a6c905d3ef347739fff
hahoyer <hahoyer@gmail.com>
2014-02-16 17:40:54 +0100

    Admin

--- HWLang/Admin/Export.h
+++ HWLang/Admin/Export.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Import.h"
 
+#include "../OpenItem.h"
 #include "../Pattern.h"
 #include "../PrioParser.h"
 #include "../PrioTable.h"
@@ -10,5 +11,4 @@
 #include "../SourcePosition.h"
 #include "../Token.h"
 #include "../TokenClass.h"
-#include "../TokenFactory.h"
 #include "../TemplateInstances.h"
