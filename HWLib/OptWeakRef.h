588e9a26aa7e0f1beb53fc852088bdf9a9c76cc9
hahoyer <hahoyer@gmail.com>
2014-03-03 10:39:34 +0100

    New: OptWeakRef for consts

--- HWLib/OptWeakRef.h
+++ HWLib/OptWeakRef.h
@@ -16,24 +16,49 @@ namespace HWLib
     public:
         using dataContainerType = T*;
         friend class WeakRef<T>;
+        friend class WeakRef<T const>;
+        friend class OptWeakRef<T const>;
     private:
-        dataContainerType _value;
+        dataContainerType value;
     public:
-        OptWeakRef() : _value() { }
-        OptWeakRef(T *value) :_value(value){ }
-        OptWeakRef(OptWeakRef<T> const&value) : _value(value.value){ };
-        OptWeakRef(WeakRef<T> const&value) : _value(value.value){ };
-        template<typename TOther>
-        OptWeakRef(OptWeakRef<TOther> const&other) : _value(other.value){ };
+        OptWeakRef() : value() { }
+        OptWeakRef(T *value) :value(value){ }
+        OptWeakRef(OptWeakRef<T const> const&value) : value(value.value){ };
+        OptWeakRef(OptWeakRef<T     > const&value) : value(value.value){ };
+        OptWeakRef(WeakRef  <T const> const&value) : value(value.value){ };
+        OptWeakRef(WeakRef <T      > const&value) : value(value.value){ };
         DefaultAssignmentOperator;
 
-        T const& operator*()const { return *_value; };
-        T const* operator->()const { return _value; };
-        T & operator*(){ return *_value; };
-        T * operator->(){ return _value; };
-        p(bool, IsValid){ return !!_value; }
+        T const& operator*()const { return *value; };
+        T const* operator->()const { return value; };
+        T     & operator*()       { return *value; };
+        T    * operator->()      { return  value; };
+        p(bool, IsValid){ return !!value; }
+    };
 
-        p(dataContainerType, value){ return _value; }
+    template<typename T>
+    class OptWeakRef<T const>
+    {
+        using thisType = OptWeakRef;
+    public:
+        using dataContainerType = T const*;
+        friend class WeakRef<T const>;
+        friend class WeakRef<T>;
+        friend class OptWeakRef<T>;
+    private:
+        dataContainerType value;
+    public:
+        OptWeakRef() : value() { }
+        OptWeakRef(T const*value) :value(value){ }
+        OptWeakRef(OptWeakRef<T const> const&value) : value(value.value){ };
+        OptWeakRef(OptWeakRef<T     > const&value) : value(value.value){ };
+        OptWeakRef(WeakRef  <T const> const&value) : value(value.value){ };
+        OptWeakRef(WeakRef <T      > const&value) : value(value.value){ };
+        DefaultAssignmentOperator;
+
+        T const& operator*()const { return *value; };
+        T const* operator->()const { return value; };
+        p(bool, IsValid){ return !!value; }
     };
 }
 
