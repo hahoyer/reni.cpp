6ba335637d780ef54e6ac4d044d7f43f2009d4cd
hahoyer <hahoyer@gmail.com>
2014-03-13 09:44:43 +0100

    New: Sign operations

--- Reni/TemplateInstances.h
+++ Reni/TemplateInstances.h
@@ -14,16 +14,30 @@ inline SearchResult const GetGenericDefinition(Type const&type){
 }
 
 template <typename T>
+inline SearchResult const GetGenericDefinition(Context const&context){
+    fd(context);
+    mb;
+}
+
+template <typename T>
 inline SearchResult const GenericFeatureClass<T>::GetDefinition(Type const&type)const{
     return GetGenericDefinition<T>(type);
+}
+
+template <typename T>
+inline SearchResult const GenericFeatureClass<T>::GetDefinition(Context const&context)const{
+    return GetGenericDefinition<T>(context);
 }
 
 template <>
 inline SearchResult const GetGenericDefinition<DumpPrintToken>(Type const&type){
     Ref<FeatureProvider<DumpPrintToken>, true> f = type;
     return f->feature;
+}
 
-    fd(type, f);
+template <>
+inline SearchResult const GetGenericDefinition<SignToken>(Context const&context){
+    fd(context);
     mb;
 }
 
