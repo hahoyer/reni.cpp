0e4db39996ecede186a18090b7fbd903e9eba5a5
hahoyer.S4M <hahoyer@gmail.com>
2014-03-24 18:27:45 +0100

    Change: split declaration and implementation to enable dependencies

--- HWAnalyse/TestMacros.h
+++ HWAnalyse/TestMacros.h
@@ -4,24 +4,26 @@
 #include <boost/preprocessor/seq/enum.hpp>
 
 #define test_class_name(name) name 
-#define test_instance_name(name) _##name 
-#define test_instance(name) HWLib::RuntimeChain<HWAnalyse::TestFixture> test_instance_name(name)(new test_class_name(name)())
-#define test_implementation(name) void test_class_name(name)::Run() const
-#define test_(name) test_class(name,{}); test_instance(name); test_implementation(name)
+#define test_instance_name(name) name::instance 
 #define test_item(name) (*test_instance_name(name).data)
 
-#define test_class(name,initialize) \
+#define test_class(name)                                             \
 struct test_class_name(name) final: public HWAnalyse::TestFixture::base{\
-    test_class_name(name)(): base(initialize){}; \
-    String const location()const override {return FILE_LINE_FUNCTION;};\
-    void Run() const override;\
+    test_class_name(name)();                                           \
+    String const location()const override;                            \
+    void Run() const override;                                       \
+    static HWLib::RuntimeChain<HWAnalyse::TestFixture> instance;                          \
 }
+
+#define test_implementation(name,initialize)                                                      \
+    test_class_name(name)::test_class_name(name)(): base(initialize){};                               \
+    String const test_class_name(name)::location()const {return FILE_LINE_FUNCTION;};                  \
+    HWLib::RuntimeChain<HWAnalyse::TestFixture> test_instance_name(name) = new test_class_name(name)(); \
+    void test_class_name(name)::Run() const
 
 #define TEST_DEPENDENY_INITIALIZE1(r,d,i,name) (test_instance_name(name).data)
 #define TEST_DEPENDENY_INITIALIZE(...) BOOST_PP_SEQ_ENUM(BOOST_PP_SEQ_FOR_EACH_I(TEST_DEPENDENY_INITIALIZE1, ~, BOOST_PP_VARIADIC_TO_SEQ(__VA_ARGS__)))
 
-#define test(name,...) \
-    test_class(name,{TEST_DEPENDENY_INITIALIZE(__VA_ARGS__)}); \
-    test_instance(name); \
-    test_implementation(name)
+#define test_(name) test_implementation(name,{})
+#define test(name,...) test_implementation(name,{TEST_DEPENDENY_INITIALIZE(__VA_ARGS__)})
 
---
+++ ReniTest/Compiler.cpp
@@ -0,0 +1,139 @@
+#include "Import.h"
+#include "Compiler.h"
+#include "SimpleTokenFactory.h"
+#include "../Reni/Syntax.h"
+#include "../HWLang/PrioTable.h"
+#include "../HWLang/PrioParser.h"
+#include "../HWAnalyse/TestMacros.h"
+#include "../HWAnalyse/TestFixture.h"
+
+using namespace HWLang;
+
+namespace _HWLang {
+
+    typedef SimpleTokenFactory TokenFactory;
+    using ScannerInstance = TokenFactory::ScannerInstance;
+    typedef TokenFactory::TokenClass TokenClass;
+    typedef TokenFactory::Syntax Syntax;
+
+    void Check(Ref<Syntax, true> const& target, bool isLeft, String const& part, bool isRight, bool isMatch);
+
+    test_(ParserBaseStructure) {
+        auto pt = PrioTable::CreateLeft({ Any }).ParenthesisLevel(Start, End);
+
+        String text = "asdf";
+        auto sc = ScannerInstance(text);
+        auto syntax = Parse<Ref<Syntax>, Ref<Syntax, true>, TokenClass, HWLang::Token<TokenClass>>(pt, sc);
+
+        a_if(!syntax.IsEmpty, nd(syntax));
+        a_if(syntax->left.IsEmpty, nd(syntax));
+        a_if(syntax->right.IsEmpty, nd(syntax));
+        a_is(syntax->name, == , text);
+    };
+
+    test(Parenthesis, ParserBaseStructure) {
+
+        String text = "({)} [(asdf)as][yxcv]";
+
+        auto pt = PrioTable::CreateLeft({ Any })
+            .ParenthesisLevel({ "(", "[", "{" }, { ")", "]", "}" })
+            .ParenthesisLevel(Start, End)
+            ;
+
+        auto sc = ScannerInstance(text);
+        auto syntax = Parse<Ref<Syntax>, Ref<Syntax, true>, TokenClass, HWLang::Token<TokenClass>>(pt, sc);
+        Check(syntax, false, "(", true, false);
+
+        auto rr = syntax->right;
+        Check(rr, true, "]", false, true);
+
+        auto rrl = rr->left;
+        Check(rrl, true, "[", true, false);
+
+        auto rrll = rrl->left;
+        Check(rrll, true, "]", false, true);
+
+        auto rrlll = rrll->left;
+        Check(rrlll, true, "[", true, false);
+
+        auto rrllll = rrlll->left;
+        Check(rrllll, true, "}", false, true);
+
+        auto rrlllll = rrllll->left;
+        Check(rrlllll, false, "{", true, false);
+
+        auto rrlllllr = rrlllll->right;
+        Check(rrlllllr, false, ")", false, false);
+
+        auto rrlllr = rrlll->right;
+        Check(rrlllr, true, "as", false, false);
+
+        auto rrlllrl = rrlllr->left;
+        Check(rrlllrl, true, ")", false, true);
+
+        auto rrlllrll = rrlllrl->left;
+        Check(rrlllrll, false, "(", true, false);
+
+        auto rrlllrllr = rrlllrll->right;
+        Check(rrlllrllr, false, "asdf", false, false);
+
+        auto rrlr = rrl->right;
+        Check(rrlr, false, "yxcv", false, false);
+    }
+
+    test(PlusTimes, ParserBaseStructure) {
+        String text = "a*b+c*d+e*f";
+
+        auto pt = PrioTable::CreateLeft({ Any })
+            .Left({ "*" })
+            .Left({ "+" })
+            .ParenthesisLevel({ "(", "[", "{" }, { ")", "]", "}" })
+            .ParenthesisLevel(Start, End)
+            ;
+
+        auto sc = ScannerInstance(text);
+        auto syntax = Parse<Ref<Syntax>, Ref<Syntax, true>, TokenClass, HWLang::Token<TokenClass>>(pt, sc);
+        Check(syntax, true, "+", true, false);
+
+        auto rl = syntax->left;
+        Check(rl, true, "*", true, false);
+
+        auto rll = rl->left;
+        Check(rll, false, "a", false, false);
+
+        auto rlr = rl->right;
+        Check(rlr, false, "b", false, false);
+
+        auto rr = syntax->right;
+        Check(rr, true, "+", true, false);
+
+
+        auto rrl = rr->left;
+        Check(rrl, true, "*", true, false);
+
+        auto rrll = rrl->left;
+        Check(rrll, false, "c", false, false);
+
+        auto rrlr = rrl->right;
+        Check(rrlr, false, "d", false, false);
+
+
+        auto rrr = rr->right;
+        Check(rrr, true, "*", true, false);
+
+        auto rrrl = rrr->left;
+        Check(rrrl, false, "e", false, false);
+
+        auto rrrr = rrr->right;
+        Check(rrrr, false, "f", false, false);
+    }
+
+    void Check(Ref<Syntax, true> const& target, bool isLeft, String const& part, bool isRight, bool isMatch) {
+        a_if(!target.IsEmpty, nd(target));
+        a_is(target->name, == , part);
+        a_if(!target->left.IsEmpty == isLeft, nd(target));
+        a_is(!target->right.IsEmpty, == , isRight);
+        a_is(target->isMatch, == , isMatch);
+    }
+}
+
