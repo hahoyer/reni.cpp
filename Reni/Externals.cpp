17f9febc238b8be294a8149f5779b6dbd7cf9d91
hahoyer <hahoyer@gmail.com>
2014-06-05 10:26:20 +0200

    Change: More external handling

---
+++ Reni/Externals.cpp
@@ -0,0 +1,50 @@
+#include "Import.h"
+#include "Externals.h"
+
+#include "External.h"
+#include "../HWLib/DumpToString.h"
+
+
+using namespace Reni;
+static bool Trace = true;
+
+External const External::This;
+External const External::Arg;
+
+bool const External::operator<(External const& other) const
+{
+    return ObjectId < other.ObjectId;
+}
+
+p_implementation(External, String, DumpHeader){
+    if(this == &This)
+        return "This";
+    if(this == &Arg)
+        return "Arg";
+    return "?";
+};
+
+
+Externals::Externals(External const& item)
+    : data({&item.thisRef})
+{
+    SetDumpString();
+}
+
+Externals::Externals(Array<WeakRef<External>> const& other)
+    : data(other)
+{
+    SetDumpString();
+}
+
+p_implementation(Externals, Array<String>, DumpData)
+{
+    return HWLib::DumpData(data);
+}
+
+
+Externals const Externals::operator+(Externals const& other) const
+{
+    auto result = data.Merge(other.data, [](WeakRef<External> left, WeakRef<External> right){return *left < *right; });
+    return  result->ToArray;
+}
---
+++ Reni/Externals.h
@@ -0,0 +1,77 @@
+#pragma once
+
+#include "../HWLib/_EditorTemplates.h"
+#include "../HWLib/DumpableObject.h"
+#include "../HWLib/WeakPtr.h"
+#include "../HWLib/CtrlRef.h"
+
+using namespace HWLib;
+using namespace std;
+
+namespace Reni
+{
+    class External; 
+
+    class Externals final : public DumpableObject
+    {
+        using baseType = DumpableObject; 
+        using thisType = Externals;
+        Array<WeakRef<External>> const data;
+    public:
+        Externals() { SetDumpString(); }
+        Externals(External const&item);
+
+    private:
+        Externals(Array<WeakRef<External>> const&other);
+    public:
+        ThisRef;
+        DefaultAssignmentOperator;
+
+        Externals const operator+(Externals const&other)const;
+    private:
+        p_function(Array<String>, DumpData) override;
+    };
+}
+
+namespace HWLib
+{
+    template<>
+    class Optional<Reni::Externals > final
+    {
+        using thisType = Optional;
+        using targetType = Reni::Externals ;
+
+        targetType const value;
+        bool const _isValid;
+    public:
+        Optional() : _isValid(false){}
+        Optional(decltype(null)) : _isValid(false){}
+        Optional(targetType const& value)
+            :_isValid(false)
+            , value(value)
+        {
+        }
+
+        p(bool, IsValid){return _isValid;}
+
+        DefaultAssignmentOperator;
+
+        p(targetType, Value){
+            a_if_(IsValid);
+            return value;
+        };
+
+        operator targetType const ()const{ return Value; };
+
+        friend thisType const operator||(thisType left, function<thisType()> right){
+            if(left.IsValid)
+                return left;
+            return right();
+        }
+        friend thisType const operator||(thisType left, thisType right){
+            if(left.IsValid)
+                return left;
+            return right;
+        }
+    };
+}
