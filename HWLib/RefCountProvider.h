167f337ce7e48e211d74d356d9a17eb6eaf55096
hahoyer <hahoyer@gmail.com>
2014-06-26 09:14:46 +0200

    Reformat

--- HWLib/RefCountProvider.h
+++ HWLib/RefCountProvider.h
@@ -1,32 +1,28 @@
 #pragma once
 
-namespace HWLib{
-    class RefCountProvider{
+namespace HWLib
+{
+    class RefCountProvider
+    {
         typedef RefCountProvider thisType;
         mutable int value;
     public:
-
-        RefCountProvider() : value(0){
-        };
-
+        RefCountProvider() : value(0){};
         RefCountProvider(RefCountProvider const&) = delete;
+        
         RefCountProvider& operator=(RefCountProvider const&) = delete;
 
-        void AddReference()const{
-            value++;
-        }
+        void AddReference()const{value++;}
 
-        void RemoveReference()const{
+        void RemoveReference()const
+        {
             a_if_(value > 0);
             value--;
             if (value)
                 return;
             delete this;
         }
-
     protected:
-        virtual ~RefCountProvider(){
-            a_if_(value == 0);
-        };
+        virtual ~RefCountProvider(){a_if_(value == 0);};
     };
 }
